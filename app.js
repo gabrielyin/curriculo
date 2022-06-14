edb20eed18863511adb4af0a3737babc4e3a8b64
gabrielyin <70323043+gabrielyin@users.noreply.github.com>
2022-06-14 12:07:49 -0300

    port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import { createTableContato } from './Controller/contato.js';
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("./frontend/"));
 app.use(express.json());
@@ -21,6 +21,6 @@ createTable();
 createTableContato();
 
 
-app.listen(port, () => {
+app.listen(`${PORT}`, () => {
     console.log("API rodando");
 });
